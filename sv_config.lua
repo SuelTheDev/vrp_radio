51ce6ec82055d7320f3cb0a2cad5bd993e5ef816
Swellington Soares <swellington.santos@gmail.com>
2022-03-22 20:44:45 -0300

    vrp rádio suel version

---
+++ sv_config.lua
@@ -0,0 +1,17 @@
+local cfg = {}
+
+cfg.radio_item = "radio"
+
+--[[
+    FRQUENCIAS PRIVADAS
+    LIMITE 1 ATÉ 9999
+    [NUM_FREQUENCIA] = "PERMISSÃO DE USO",
+]]
+
+cfg.frequency = {
+    [1] = "player.givemoney",
+    [2] = "player.givemoney",
+    [3] = "player.givemoney"
+}
+
+return cfg
